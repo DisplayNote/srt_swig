ae6a76a1c3a2f5ffdb0eea9a5b86aa857d6213dd
Lewis Kirkaldie <lewis@gmail.com>
2019-12-09 20:30:30 +0000

    -attempt to build with dotnet the interop -reset nuspec naming to srtsharp

--- scripts/create-win64-nuget.bat
+++ scripts/create-win64-nuget.bat
@@ -1,12 +1,18 @@
 rem Bundle CI NuGet package for VS2015 Win64 Release only build (true package should contain multi-arch composite output)
 @echo off
 
-IF "%PLATFORM%"=="x64" (
-    IF "%VS_VERSION%"=="2015" (
-        IF "%CONFIGURATION%"=="Release" (
-            nuget pack .\scripts\nuget\SRT.Library.TempDevId\SRT.Library.TempDevId.nuspec -version %APPVEYOR_BUILD_VERSION%
-            appveyor PushArtifact SRT.Library.TempDevId.%APPVEYOR_BUILD_VERSION%.nupkg
-            exit 0
+rem If running in AppVeyor, only create a NuGet package on VS2015 x64 Release
+IF DEFINED APPVEYOR_BUILD_VERSION (
+    IF "%PLATFORM%"=="x64" (
+        IF "%VS_VERSION%"=="2015" (
+            IF "%CONFIGURATION%"=="Release" (
+                nuget pack .\scripts\nuget\SrtSharp\SrtSharp.nuspec -version %APPVEYOR_BUILD_VERSION%-beta
+                appveyor PushArtifact SRT.Library.TempDevId.%APPVEYOR_BUILD_VERSION%.nupkg
+                exit 0
+            )
         )
-    )
-) 
+    ) 
+) ELSE (
+    rem probably running on a local workstation, so use the first given argument parameter as the version number and don't push or exit
+    nuget pack .\scripts\nuget\SrtSharp\SrtSharp.nuspec -version %1
+)
