c790f5242fd2ad9b432e96ab4ab6a4610fdd2e7e
Lewis Kirkaldie <lewis@cinegy.com>
2019-12-09 15:51:53 +0000

    -run CI inside _build sub-folder -fix up scripts for nuget packing

--- scripts/create-win64-nuget.bat
+++ scripts/create-win64-nuget.bat
@@ -4,7 +4,7 @@ rem Bundle CI NuGet package for VS2015 Win64 Release only build (true package sh
 IF "%PLATFORM%"=="x64" (
     IF "%VS_VERSION%"=="2015" (
         IF "%CONFIGURATION%"=="Release" (
-            nuget pack .\scripts\nuget\srtlib\srtlib.nuspec -version %APPVEYOR_BUILD_VERSION%
+            nuget pack .\scripts\nuget\SRT.Library.TempDevId\SRT.Library.TempDevId.nuspec -version %APPVEYOR_BUILD_VERSION%
             appveyor PushArtifact SRT.Library.TempDevId.%APPVEYOR_BUILD_VERSION%.nupkg
             exit 0
         )
